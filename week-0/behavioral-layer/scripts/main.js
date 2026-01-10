f34f4364f406aff81350d7f5c68d3400e98cfd94
Jaeden Crow <jivescrow@ewu.edu>
2026-01-10 19:20:52 +0000

    Add behavioral layer lab

--- week-0/behavioral-layer/scripts/main.js
+++ week-0/behavioral-layer/scripts/main.js
@@ -1,4 +1,49 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/fox1.png") {
+    myImage.setAttribute("src", "images/fox2.png");
+  } else {
+    myImage.setAttribute("src", "images/fox1.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Can you see it, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Can you see it, ${myName}`;
+  }
+}
+
+/*
+  SUMMARY:
+  This file does two things:
+  1. It changes the image when the user clicks on it.
+  2. It sets a personalized welcome message based on the user's name.
+​
+  The key pattern I learned: Using event listeners and localStorage to create interactive and personalized web experiences.
+*/
