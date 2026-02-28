69dda9db1a4c36144356b2575e99ac31f1a58fc2
“m-crow-m” <jaedenives@yahoo.com>
2026-02-28 14:11:48 -0800

    Redesign page with brutal aesthetic

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,1 @@
-// =============================================
-// DESN 378: Code + Design 2
-// Your JavaScript will live here.
-//
-// This file will grow throughout the quarter
-// as you learn to make the web behave.
-// =============================================
-
-console.log('Portfolio loaded');
-
-//week 0 hello world
- alert("I'm Javascript!");
-// Week 1: You'll add theme toggle code here
-// Week 2: You'll add localStorage persistence here
-// Week 3+: More to come...
+console.log("DESN 378 homepage loaded");
